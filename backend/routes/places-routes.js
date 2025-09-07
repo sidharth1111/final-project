59e57a68d7bf18ad8c8f20e14fc0b850025a18ae
sidharth <sidharth.delhi12@gmail.com>
2025-09-07 21:51:44 +0530

    Add createPlace POST API

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,46 +1,12 @@
 const express = require('express');
-const HttpError = require("../models/http-error")
+const placesControllers = require("../controllers/places-controller")
 
 const router = express.Router();
 
-const dummy_places = [
-    {
-        id: 'p1',
-        title: "Taj Mahal",
-        description: "Beautiful",
-        location: {
-            lat: 34,
-            lng: -84
-        },
-        address: 20,
-        creator: 'u1'
-    }
-]
+router.get("/:pid", placesControllers.getPlaceById);
 
-router.get("/:pid", (req, res, next) => {
-    const placeId = req.params.pid;
+router.get("/user/:uid", placesControllers.getPlaceByUserId);
 
-    const place = dummy_places.find(p => {
-        return p.id === placeId;
-    });
+router.post("/", placesControllers.createPlace)
 
-    if (!place) {
-        throw new HttpError("Coudn't find a place for the provided place id.", 500);
-    }
-
-    res.json({place: place});
-});
-
-router.get("/user/:uid", (req, res, next) => {
-    const uid = req.params.uid;
-    console.log(uid)
-    const place = dummy_places.find(p => {
-        return uid === p.creator;
-    });
-
-    if (!place) {
-        return next(new HttpError("Couldn't find a place for the provided user id.", 404));
-    }
-    res.json({place})
-});
 module.exports = router;
